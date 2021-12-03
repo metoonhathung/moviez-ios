e25e6353ee307d40d7491c04ac1b0846310509a9
metoonhathung <nhathungmetoo@gmail.com>
2021-12-03 18:56:00 -0500

    Initial Commit

---
+++ Moviez/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moviez
+//
+//  Created by Trần Ngô Nhật Hưng on 12/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
